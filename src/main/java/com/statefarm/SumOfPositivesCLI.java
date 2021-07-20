382c87b1c6068fa8bd66343ada0eb10d8b003deb
Casey Cook <casey.cook.eher@statefarm.com>
2021-07-20 09:22:17 -0500

    add working code for calculating sum

--- src/main/java/com/statefarm/SumOfPositivesCLI.java
+++ src/main/java/com/statefarm/SumOfPositivesCLI.java
@@ -1,4 +1,17 @@
 package com.statefarm;
 
 public class SumOfPositivesCLI {
+
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            int currentValue = Integer.valueOf(args[i]);
+            if (currentValue >= 0) {
+                sum += currentValue;
+            }
+        }
+        System.out.println(sum);
+
+    }
 }
